75c55d425180165b13f92c0ef79edccfca3b850e
Sahil Savaj <sahil.savaj@respo.co.in>
2026-06-22 19:16:56 +0530

    feat: Added internet available check

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,10 @@ module.exports = {
         {
             name: "uptime-kuma",
             script: "./server/server.js",
+            env: {
+                // Skip health checks while the laptop has no network (macOS scutil --nwi)
+                UPTIME_KUMA_SKIP_CHECKS_WHEN_OFFLINE: "1",
+            },
         },
     ],
 };
